4eb06cf1e656f77165f5c9d6f996c936826c7de3
MdAmine <saoud93@gmail.com>
2016-10-05 14:02:38 +0100

    Add a Navigation Drawer and a Scroll Tabs

--- app/src/main/java/com/mdamine/movieapp/MainActivity.java
+++ app/src/main/java/com/mdamine/movieapp/MainActivity.java
@@ -1,13 +1,107 @@
 package com.mdamine.movieapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.mdamine.movieapp.adapters.ScrollTabsAdapter;
+import com.mdamine.movieapp.fragments.BoxOfficeFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private List<String> titleList = new ArrayList<>();
+
+    private Toolbar toolbar;
+
+    private ViewPager viewPager;
+    private ScrollTabsAdapter adapter;
+    private TabLayout tabLayout;
+
+    private DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Main");
+
+        setUpScrollTabs();
+        setUpDrawer();
+
+    }
+
+
+    private void setUpScrollTabs() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+
+        fragmentList.add(new BoxOfficeFragment());
+        titleList.add("BoxOffice");
+
+        for (int i = 0; i < 3; i++) {
+            fragmentList.add(new Fragment());
+            titleList.add("Fragment " + i);
+        }
+
+        adapter = new ScrollTabsAdapter(getSupportFragmentManager(), fragmentList, titleList);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setUpDrawer() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        String itemName = (String) item.getTitle();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        Toast.makeText(this, itemName, Toast.LENGTH_SHORT).show();
+
+        return true;
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
     }
 }
---
+++ app/src/main/java/com/mdamine/movieapp/adapters/ScrollTabsAdapter.java
@@ -0,0 +1,38 @@
+package com.mdamine.movieapp.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by SAOUD Mohamed Amine on 05/10/2016.
+ */
+
+public class ScrollTabsAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragmentList;
+    private List<String> titleList;
+
+    public ScrollTabsAdapter(FragmentManager fm, List<Fragment> fragmentList, List<String> titleList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+        this.titleList = titleList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titleList.get(position);
+    }
+}
